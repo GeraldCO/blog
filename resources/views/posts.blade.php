75a0ab1ea5c8cbb0fa65c729641760102b650e5e
gerald <geraldcerd@gmail.com>
2022-06-19 22:17:48 -0600

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,13 @@
+<x-layout >
+    <x-slot name="content">
+        @foreach($posts as $post)
+        <article>
+            <a href="/posts/{{ $post->slug }} ">
+                <h1> <?=  $post->title  ?> </h1>
+            </a>            
+            <div> {!! $post->excerpt !!} </div>
+        </article>
+
+    @endforeach
+    </x-slot>
+</x-layout>
